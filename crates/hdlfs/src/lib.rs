f9c6aa97ef9ccc9f44371aeafc5728039248c8ea
Simon <simon.yang@sap.com>
2025-06-19 16:30:27 +0800

    init

---
+++ crates/hdlfs/src/lib.rs
@@ -0,0 +1,90 @@
+use std::collections::HashMap;
+use std::str::FromStr;
+use std::sync::Arc;
+
+use deltalake_core::logstore::{
+    default_logstore, logstore_factories, object_store_factories, LogStore, LogStoreFactory,
+    ObjectStoreFactory, ObjectStoreRef, StorageConfig,
+};
+use deltalake_core::{DeltaResult, DeltaTableError, Path};
+use object_store::hdlfs::{SAPHdlfsBuilder, SAPHdlfsConfigKey};
+use object_store::{ObjectStoreScheme, RetryConfig};
+use tokio::runtime::Handle;
+use url::Url;
+
+mod config;
+mod error;
+
+trait HdlfsOptions {
+    fn as_hdlfs_options(&self) -> HashMap<SAPHdlfsConfigKey, String>;
+}
+
+impl HdlfsOptions for HashMap<String, String> {
+    fn as_hdlfs_options(&self) -> HashMap<SAPHdlfsConfigKey, String> {
+        self.iter()
+            .filter_map(|(key, value)| {
+                Some((
+                    SAPHdlfsConfigKey::from_str(&key.to_ascii_lowercase()).ok()?,
+                    value.clone(),
+                ))
+            })
+            .collect()
+    }
+}
+
+#[derive(Clone, Default, Debug)]
+pub struct HdlfsFactory {}
+
+impl ObjectStoreFactory for HdlfsFactory {
+    fn parse_url_opts(
+        &self,
+        url: &Url,
+        options: &HashMap<String, String>,
+        retry: &RetryConfig,
+        handle: Option<Handle>,
+    ) -> DeltaResult<(ObjectStoreRef, Path)> {
+        let config = config::SAPHdlfsConfigHelper::try_new(options.as_hdlfs_options())?.build()?;
+
+        let mut builder = SAPHdlfsBuilder::new()
+            .with_url(url.to_string())
+            .with_retry(retry.clone());
+        for (key, value) in config.iter() {
+            builder = builder.with_config(*key, value.clone());
+        }
+        let store = builder.build()?;
+
+        let (_, path) =
+            ObjectStoreScheme::parse(url).map_err(|e| DeltaTableError::GenericError {
+                source: Box::new(e),
+            })?;
+        let prefix = Path::parse(path)?;
+
+        Ok((Arc::new(store), prefix))
+    }
+}
+
+impl LogStoreFactory for HdlfsFactory {
+    fn with_options(
+        &self,
+        prefixed_store: ObjectStoreRef,
+        root_store: ObjectStoreRef,
+        location: &Url,
+        options: &StorageConfig,
+    ) -> DeltaResult<Arc<dyn LogStore>> {
+        Ok(default_logstore(
+            prefixed_store,
+            root_store,
+            location,
+            options,
+        ))
+    }
+}
+
+/// Register an [ObjectStoreFactory] for common Hdlfs [Url] schemes
+pub fn register_handlers(_additional_prefixes: Option<Url>) {
+    let factory = Arc::new(HdlfsFactory {});
+    let scheme = &"hdlfs";
+    let url = Url::parse(&format!("{scheme}://")).unwrap();
+    object_store_factories().insert(url.clone(), factory.clone());
+    logstore_factories().insert(url.clone(), factory.clone());
+}
